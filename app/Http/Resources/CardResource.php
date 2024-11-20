2e8d4ab04f1caa1c66302b2a786fdfe5aa552944
NubbbZ <4251461+NubbbZ@users.noreply.github.com>
2024-11-20 00:51:19 -0500

    added card attributes to the resource

--- app/Http/Resources/CardResource.php
+++ app/Http/Resources/CardResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\CardElement;
 use App\Models\Set;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -15,12 +16,17 @@ class CardResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
+        $data = array(
             'name' => $this->name,
             'image' => $this->image,
             'set' => Set::find($this->set_id)->name,
             'reference' => $this->reference,
-            //'attributes' => $this->attributes,
-        ];
+        );
+
+        foreach (CardElement::all() as $element) {
+            $data[$element->slug] = $this->card_elements[$element->slug] ?? null;
+        }
+
+        return $data;
     }
 }
