98ce8d1b7fd78e1c42d2db53785f47f249cf801c
NubbbZ <4251461+NubbbZ@users.noreply.github.com>
2024-12-12 10:36:22 -0500

    hidden user api request from scribe docs

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+/**
+ * @hideFromAPIDocumentation
+ */
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
